0a08c24821cfd8e928c5d185687abb5836d274a0
AmarNagim <89778585+AmarNagim@users.noreply.github.com>
2021-11-01 10:18:42 +0100

    working lightswitch

--- lightswitch.py
+++ lightswitch.py
@@ -1,11 +1,34 @@
 import tkinter as tk
 window = tk.Tk()
 
-button = tk.Button(text='...', bg="white", fg="black")
-button.pack(pady = 20, padx = 20)
+
+
+print('De lamp is uit..')
 
 # schijf hier tussen je code
+window.configure (background="black")
 
+currentState = 'off'
+# currentStateSwitch = True
+
+# while currentStateSwitch == True:
+
+text = 'off'
+def onclick():
+    global currentState
+    if currentState == 'on':
+        button['text'] = 'Switch light on'
+        print('De lamp is uit..')
+        window.configure (background="black")
+        currentState = 'off' 
+    elif currentState == 'off':   
+        button['text'] = 'Switch light off'
+        print('De lamp is aan..')
+        window.configure (background="yellow")
+        currentState = 'on'  
+                    
+button = tk.Button(text=f'Switch light on', bg="white", fg="black", command=onclick)
+button.pack(pady = 20, padx = 20)
 
 
 # schijf hier tussen je code
